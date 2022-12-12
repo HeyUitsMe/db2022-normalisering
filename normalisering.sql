ec1bbca7576f9b4c2bf074ee4aa530f28a878a2a
HeyUitsMe <rimajohnson.dev@gmail.com>
2022-12-12 10:59:02 +0100

    Adding script

---
+++ normalisering.sql
@@ -0,0 +1,38 @@
+USE iths;
+
+DROP TABLE IF EXISTS UNF;
+
+CREATE TABLE `UNF` (
+    `Id` DECIMAL(38, 0) NOT NULL,
+    `Name` VARCHAR(26) NOT NULL,
+    `Grade` VARCHAR(11) NOT NULL,
+    `Hobbies` VARCHAR(25),
+    `City` VARCHAR(10) NOT NULL,
+    `School` VARCHAR(30) NOT NULL,
+    `HomePhone` VARCHAR(15),
+    `JobPhone` VARCHAR(15),
+    `MobilePhone1` VARCHAR(15),
+    `MobilePhone2` VARCHAR(15)
+)  ENGINE=INNODB;
+
+LOAD DATA INFILE '/var/lib/mysql-files/denormalized-data.csv'
+INTO TABLE UNF
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+DROP TABLE IF EXISTS Student;
+
+CREATE TABLE Student (
+    Id INT NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    CONSTRAINT PRIMARY KEY (Id)
+)  ENGINE=INNODB;
+
+INSERT INTO Student (ID, FirstName, LastName) 
+SELECT DISTINCT Id, SUBSTRING_INDEX(Name, ' ', 1), SUBSTRING_INDEX(Name, ' ', -1) 
+FROM UNF;
+
