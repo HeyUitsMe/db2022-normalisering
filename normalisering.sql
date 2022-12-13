1c6d19b84cde61a626f8fe5ea50cd9ef57baf553
HeyUitsMe <rimajohnson.dev@gmail.com>
2022-12-13 14:49:31 +0100

    tagit bort kopplingstabellen

--- normalisering.sql
+++ normalisering.sql
@@ -50,16 +50,3 @@ ALTER TABLE School MODIFY COLUMN SchoolId Int AUTO_INCREMENT;
 INSERT INTO School(Name, City) values ('Rimas Skola','Solna');
 
 
-DROP TABLE IF EXISTS StudentSchool;
-
-CREATE TABLE StudentSchool AS SELECT DISTINCT UNF.Id, School.SchoolId 
-from UNF INNER JOIN School ON UNF.School = School.Name;
-
-ALTER TABLE StudentSchool ADD PRIMARY KEY(Id, SchoolId);
-
-SELECT Id, FirstName, LastName FROM Student
-JOIN StudentSchool USING (Id);
-
- SELECT Id, FirstName, LastName, Name, City FROM Student JOIN StudentSchool USING (Id)  JOIN School USING (SchoolId);
-
-
