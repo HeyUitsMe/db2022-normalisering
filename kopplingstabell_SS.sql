ab8a284dfdd860b0fa7337d6e55fad1d75d60bff
HeyUitsMe <rimajohnson.dev@gmail.com>
2022-12-13 14:45:45 +0100

    first commit on kopplingstabell_SS

---
+++ kopplingstabell_SS.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS StudentSchool;
+
+CREATE TABLE StudentSchool AS SELECT DISTINCT UNF.Id, School.SchoolId
+from UNF INNER JOIN School ON UNF.School = School.Name;
+
+ALTER TABLE StudentSchool ADD PRIMARY KEY(Id, SchoolId);
+
+SELECT Id, FirstName, LastName FROM Student
+JOIN StudentSchool USING (Id);
+
+SELECT Id, FirstName, LastName, Name, City FROM Student JOIN StudentSchool USING (Id) JOIN School USING (SchoolId);
